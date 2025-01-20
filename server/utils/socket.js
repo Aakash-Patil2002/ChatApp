ebb4b4b7ce1d74b1ce6738c99804419a59faa1c2
Aakash Patil <akashnp25274@gmail.com>
2025-01-20 14:35:51 +0530

    Update socket.js

--- server/utils/socket.js
+++ server/utils/socket.js
@@ -9,7 +9,7 @@ const server=http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: ["https://aakashpatil-chatapp.netlify.app"]
+        origin: ["https://localhost:3000"]
     }
 });
 
